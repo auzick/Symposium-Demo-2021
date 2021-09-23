2c6a5fe896d620980a19a7e84af7c26ba9ab58ac
auzick <andy@uzick.com>
2021-09-23 14:21:51 -0500

    change to spot

--- src/rendering/src/components/SimpleSpot.tsx
+++ src/rendering/src/components/SimpleSpot.tsx
@@ -1,10 +1,9 @@
-import { Text, Field, Image, Link, RichText, ImageField, LinkField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Text, Field, Link, RichText, ImageField, LinkField } from '@sitecore-jss/sitecore-jss-nextjs';
 import { StyleguideComponentProps } from 'lib/component-props';
 
 type BasicSpotProps = StyleguideComponentProps & {
   fields: {
     heading: Field<string>;
-    image: ImageField
     link: LinkField;
     bodytext: Field<string>;
   };
@@ -12,16 +11,13 @@ type BasicSpotProps = StyleguideComponentProps & {
 
 const BasicSpot = (props: BasicSpotProps): JSX.Element => (
   <div style={{ float: "left", height: "600px", marginBottom: "24px" }} className="g-round-card__wrap col-md-6 col-sm-6 col-xs-12">
-    <div className="g-round-card " style={{ border: "solid 1px black", paddingTop: "22px", paddingBottom: "22px" }}>
+    <div
+      className="g-round-card "
+      style={{  border: "solid 1px black", paddingTop: "22px", paddingBottom: "22px" }}
+    >
       <div className="g-round-card__inner">
         <div className="g-round-card__content">
           <Text tag="h2" style={{ marginLeft: "22px", marginRight: "22px" }} className="g-round-card__heading" field={props?.fields?.heading} />
-          <Image
-            height="240"
-            width="390"
-            style={{ marginLeft: "22px", marginRight: "22px" }}
-            field={props?.fields?.image}
-          />
           <RichText style={{ fontSize: "1.6rem", marginLeft: "22px", marginRight: "22px" }} field={props?.fields?.bodytext} />
           <span className="g-round-card__foot">
             <Link style={{ color: "Red", marginLeft: "22px", marginBottom:"4px" }} field={props?.fields?.link}  />
