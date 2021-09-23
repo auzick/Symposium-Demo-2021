6f7cd339b85b49944c0e2add4af09e7d275bed50
auzick <andy@uzick.com>
2021-09-23 13:15:22 -0500

    simple spot

--- src/rendering/src/components/SimpleSpot.tsx
+++ src/rendering/src/components/SimpleSpot.tsx
@@ -11,15 +11,20 @@ type BasicSpotProps = StyleguideComponentProps & {
 };
 
 const BasicSpot = (props: BasicSpotProps): JSX.Element => (
-  <div style={{ float: "left", height: "600px", marginBottom: "24px" }} className="g-round-card__wrap col-md-4 col-sm-6 col-xs-12">
-    <div className="g-round-card ">
+  <div style={{ float: "left", height: "600px", marginBottom: "24px" }} className="g-round-card__wrap col-md-6 col-sm-6 col-xs-12">
+    <div className="g-round-card " style={{ border: "solid 1px black", paddingTop: "22px", paddingBottom: "22px" }}>
       <div className="g-round-card__inner">
         <div className="g-round-card__content">
-          <Text tag="p" className="g-round-card__heading" field={props?.fields?.heading} />
-          <Image style={{ height: "305px" }} field={props?.fields?.image} />
+          <Text tag="h2" style={{ marginLeft: "22px", marginRight: "22px" }} className="g-round-card__heading" field={props?.fields?.heading} />
+          <Image
+            height="240"
+            width="390"
+            style={{ marginLeft: "22px", marginRight: "22px" }}
+            field={props?.fields?.image}
+          />
           <RichText style={{ fontSize: "1.6rem", marginLeft: "22px", marginRight: "22px" }} field={props?.fields?.bodytext} />
           <span className="g-round-card__foot">
-            <Link style={{ color: "Red" }} field={props?.fields?.link}  />
+            <Link style={{ color: "Red", marginLeft: "22px", marginBottom:"4px" }} field={props?.fields?.link}  />
             {/* <a style={{ color: "Red" }} href={props?.fields?.link?.value.href}>Read more</a> */}
           </span>
         </div>
